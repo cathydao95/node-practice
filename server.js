dc0d0d9e31c2676ef19703c00e4fcb2641211437
Cathy Dao <cathydao95@gmail.com>
2022-06-09 22:16:15 -0700

    changes

--- server.js
+++ server.js
@@ -38,6 +38,6 @@ app.get("/api/:personName", (request, response) => {
   }
 });
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
   console.log(`The server is running on ${PORT}`);
 });
