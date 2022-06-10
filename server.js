5cf74ae0586e0b64c9e5c033c6b93a89bc553569
Cathy Dao <cathydao95@gmail.com>
2022-06-09 21:03:10 -0700

    add gitignore and js and html files

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const PORT = 8000;
+
+const people = {
+  "cathy d": {
+    age: 27,
+    birthName: "Cathy D",
+    birthLocation: "Fountain Valley, California",
+  },
+  "bryan d": {
+    age: 17,
+    birthName: "Bryan D",
+    birthLocation: "Torrance, California",
+  },
+  "kaitlyn m": {
+    age: 31,
+    birthName: "Hien D",
+    birthLocation: "Saigon, Vietnam",
+  },
+  other: {
+    age: 5,
+    birthName: "Other",
+    birthLocation: "Other",
+  },
+};
+
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/index.html");
+});
+
+app.get("/api/:personName", (request, response) => {
+  const personsName = request.params.personName.toLowerCase();
+  if (people[personsName]) {
+    response.json(people[personsName]);
+  } else {
+    response.json(people["other"]);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`The server is running on ${PORT}`);
+});
